616fc5d9c8a0d97335e9e18030ba07c06820f813
RedHeartA <zsyw@xxx.local>
2017-08-29 08:46:08 +0800

    fff

--- AVPlayerDemo/AppDelegate.m
+++ AVPlayerDemo/AppDelegate.m
@@ -21,7 +21,6 @@
     self.window = [[UIWindow alloc] init];
     ViewController * vc = [[ViewController alloc] init];
     self.window.rootViewController = vc;
-    //初始化
     [self.window makeKeyAndVisible];
     return YES;
 }
